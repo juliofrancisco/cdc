c66ced0ca6bfbc66b560a5758cef5b6661452d45
Julio Francisco Vicente Sánchez <jfvicente@ayesa.com>
2024-10-08 12:18:14 +0200

    solucionados errores compilación

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../../front/src/Home';
+import Home from '../../frontend/src/Home';
 import Noticias from './Noticias';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
